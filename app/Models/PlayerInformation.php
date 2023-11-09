748e15cbc6fa9ed05c863254228770cc8d9d49d1
kkamara <kamaracomputers@gmail.com>
2023-11-09 12:51:48 +0000

    Add PlayerInformation::getPlayerInformation()

---
+++ app/Models/PlayerInformation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Traits\Tappable;
+use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Client;
+
+class PlayerInformation extends Model
+{
+    use HasFactory;
+    use Tappable;
+
+    public function getPlayerInformation(string $playerId) {
+        try {
+            $client = new Client();
+
+            $response = $client->request(
+                'GET', 
+                'https://'.config("app.rapid_api_host").'/getMLBPlayerInfo?getStats=false&playerId='.$playerId, 
+                ['headers' => [
+                    'X-RapidAPI-Host' => config("app.rapid_api_host"),
+                    'X-RapidAPI-Key' => config("app.rapid_api_key"),
+                ]]
+            );
+
+            return $response->getBody();
+        } catch (RequestException $exception) {
+            Log::error($exception->getMessage());
+            return false;
+        }
+    }
+}
